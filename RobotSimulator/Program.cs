45eb74df88cba607c4dbbd1b58e2bdd4792d3f19
Brian <brian@Brians-MacBook-Pro---M3pro.local>
2025-02-17 11:57:25 +0800

    create game loop

--- RobotSimulator/Program.cs
+++ RobotSimulator/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Welcome to the Robot Simulator!");
+        Console.WriteLine("Commands: PLACE, MOVE, LEFT, RIGHT, REPORT");
+        Console.WriteLine("Type 'EXIT' to quit the program.");
+
+        while (true)
+        {
+            //Console.WriteLine("Enter a command: ");
+            var command = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(command) && command.ToUpper() == "EXIT")
+            {
+                break;
+            }
+
+            Console.WriteLine($"You entered: {command}");
+        }
     }
 }
